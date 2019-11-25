8400ed6978369faffd081718a980257b4043330c
Niels van Kampenhout <niels.vankampenhout@bloomreach.com>
2019-11-25 10:30:04 -0800

    Configure Image Variants

---
+++ site/components/src/main/java/org/example/beans/Imageset.java
@@ -0,0 +1,10 @@
+package org.example.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
+
+@HippoEssentialsGenerated(internalName = "gogreen:imageset")
+@Node(jcrType = "gogreen:imageset")
+public class Imageset extends HippoGalleryImageSet {
+}
